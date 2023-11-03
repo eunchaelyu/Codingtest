384f48d3b4c58e9b3300134bd50fed5cdd08481c
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2023-11-03 16:37:00 +0900

    Create 특수문자 출력하기.java

---
+++ Programmers/특수문자 출력하기.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;"); // \나 "앞에는 \를 붙여서 그대로 출력
+    }
+}
