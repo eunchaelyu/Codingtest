ec361a9426dc72a252cd9da4e85142f498afc04f
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2023-12-15 20:14:56 +0900

    Create 프로그래머스 Lv.0 문자열을 정수로 바꾸기.java

---
+++ Programmers/프로그래머스 Lv.0 문자열을 정수로 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer=Integer.parseInt(s); //정수 객체로 리턴
+        
+        //answer=Integer.valueOf(s); //기본형 정수로 리턴 
+        return answer;
+    }
+}
