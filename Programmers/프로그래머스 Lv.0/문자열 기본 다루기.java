0ec1f994526f3fb5411e9610f0de66ff84b1bbc1
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2024-01-03 09:34:50 +0900

    Update 문자열 기본 다루기.java

--- Programmers/프로그래머스 Lv.0/문자열 기본 다루기.java
+++ Programmers/프로그래머스 Lv.0/문자열 기본 다루기.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        for (char c : s.toCharArray()) {
-            if (!Character.isDigit(c)) {
-                return false;
+        if(s.length()==4 || s.length()==6){
+            for (char c : s.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
             }
-        }
+        } else return false;
         return answer;
     }
 }
