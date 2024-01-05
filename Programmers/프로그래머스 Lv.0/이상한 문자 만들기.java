bff21f966e74efc2b0ebf5b524ad631ec56e8a56
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2024-01-05 10:08:17 +0900

    Update 이상한 문자 만들기.java

--- Programmers/프로그래머스 Lv.0/이상한 문자 만들기.java
+++ Programmers/프로그래머스 Lv.0/이상한 문자 만들기.java
@@ -1,10 +1,22 @@
 class Solution {
     public String solution(String s) {
-        char[] charArray=s.toCharArray();
-        for(int i=0; i<charArray.length; i++){ 
-            if(i%2 ==0) 
-                charArray[i]=Character.toUpperCase(charArray[i]);
+        String answer="";
+        int count=0;
+        for(int i=0; i<s.length();i++){
+            char word=s.charAt(i);
+            if(word==' '){
+                answer += " ";
+                count = 0;
+                continue;
+            }
+            if((count%2)==0){
+                answer+=String.valueOf(Character.toUpperCase(word));
+                count++;
+            }else{
+                answer+=String.valueOf(Character.toLowerCase(word));
+                count++;
+            }
         }
-        return new String(charArray);
+        return answer;
     }
 }
