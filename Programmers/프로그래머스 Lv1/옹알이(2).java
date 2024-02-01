dba4b3c63655e881b82f3e32b945c0ae6de28ff4
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2024-02-01 14:15:31 +0900

    Update 옹알이(2).java

--- Programmers/프로그래머스 Lv1/옹알이(2).java
+++ Programmers/프로그래머스 Lv1/옹알이(2).java
@@ -1,23 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
 
-        // 입력된 각각의 단어에 대해 반복
-        for (String word : babbling) {
-            boolean isValid = true;
+        for (int i = 0; i < babbling.length; i++) {
+            // 중복 발음 대체
+            babbling[i] = babbling[i].replaceAll("(ayaaya|yeye|woowoo|mama)", "1");
+            // 나머지 발음 대체
+            babbling[i] = babbling[i].replaceAll("(aya|ye|woo|ma)", " ");
 
-            // 단어 내 연속하는 같은 문자를 찾기 위한 루프
-            for (int i = 1; i < word.length(); i++) {
-                // 현재 문자와 이전 문자가 같으면 false 처리
-                if (word.charAt(i) == word.charAt(i - 1)) {
-                    isValid = false;
-                    break;
-                }
-            }
-
-            // 가능한 발음이면 카운트함
-            if (isValid && (word.contains("aya") || word.contains("ye") || word.contains("woo") || word.contains("ma"))) {
-                answer++;
+            // 문자열이 모두 공백인 경우 유효한 발음
+            if (babbling[i].trim().isEmpty()) {
+                ans++;
             }
         }
 
