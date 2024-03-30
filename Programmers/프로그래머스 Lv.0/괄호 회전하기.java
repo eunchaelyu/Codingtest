107f2c41b4420c08c538025032b6f567b780f6bc
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2024-03-30 13:01:15 +0900

    Update 괄호 회전하기.java

--- Programmers/프로그래머스 Lv.0/괄호 회전하기.java
+++ Programmers/프로그래머스 Lv.0/괄호 회전하기.java
@@ -1,11 +1,31 @@
-class Solution {
-    public String solution(String my_string, int[] index_list) {
-        String answer = "";
-        
-        for(int i=0; i<index_list.length; i++){
-            answer += my_string.charAt(index_list[i]);
+import java.util.Stack;
+
+public class Solution {
+    static public int solution(String s) {
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
+            String str = s.substring(i, s.length()) + s.substring(0, i);
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                answer++;
+            }
         }
-        
+
         return answer;
     }
 }
